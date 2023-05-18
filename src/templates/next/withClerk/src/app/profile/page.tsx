c2118a7851208c9a155e24dd949a3fb24b192016
Kasidis Chantharojwong <me@ckasidis.com>
2023-05-19 02:15:04 +0700

    fix: update next withClerk template

--- src/templates/next/withClerk/src/app/profile/page.tsx
+++ src/templates/next/withClerk/src/app/profile/page.tsx
@@ -1,12 +1,10 @@
 import { currentUser } from '@clerk/nextjs'
 
 export default async function ProfilePage() {
-  const { firstName, lastName, id } = await currentUser()
+  const { id } = await currentUser()
   return (
     <div className="mx-auto grid max-w-6xl gap-y-4 px-6 py-4">
-      <h1 className="mt-4 text-2xl">
-        {firstName} {lastName}
-      </h1>
+      <h1 className="mt-4 text-2xl">Profile</h1>
       <p>id: {id}</p>
     </div>
   )
